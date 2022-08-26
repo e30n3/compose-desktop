e704c2190effdf6bcc9aa77c5db5124c67e46dd8
Ivan Zaytsev <e30n3@yandex.ru>
2022-08-26 14:31:50 +0300

    feat: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,120 +3,127 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.6.10"
-    kotlin("jvm") version kotlinVersion
-    kotlin("kapt") version kotlinVersion
-    id("org.jetbrains.compose") version "1.1.1"
+  val kotlinVersion = "1.6.10"
+  kotlin("jvm") version kotlinVersion
+  kotlin("kapt") version kotlinVersion
+  id("org.jetbrains.compose") version "1.1.1"
 }
 kapt {
-    correctErrorTypes = true
+  correctErrorTypes = true
 }
 
 group = "com.myapp"
 version = "1.0.0"
 
 repositories {
-    jcenter()
-    mavenCentral()
-    google()
-    maven { url = uri("https://jitpack.io") }
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+  jcenter()
+  mavenCentral()
+  google()
+  maven { url = uri("https://jitpack.io") }
+  maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
 }
 
 val daggerVersion by extra("2.43.2")
 
 dependencies {
-    implementation(compose.desktop.currentOs)
-    implementation(compose.materialIconsExtended)
+  implementation(compose.desktop.currentOs)
+  implementation(compose.materialIconsExtended)
 
-    // Module dependencies
-    implementation(project(":data"))
-    // Dagger : A fast dependency injector for Android and Java.
-    /*  kapt("com.google.dagger:dagger-compiler:$daggerVersion")
-      kaptTest("com.google.dagger:dagger-compiler:$daggerVersion")*/
-    implementation("com.google.dagger:dagger:$daggerVersion")
-    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+  // Module dependencies
+  implementation(project(":data"))
+  // Dagger : A fast dependency injector for Android and Java.
+  /*  kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+    kaptTest("com.google.dagger:dagger-compiler:$daggerVersion")*/
+  implementation("com.google.dagger:dagger:$daggerVersion")
+  kapt("com.google.dagger:dagger-compiler:$daggerVersion")
 
-    // Cyclone : https://github.com/theapache64/cyclone
-    implementation("com.theapache64:cyclone:1.0.0-alpha01")
+  // Cyclone : https://github.com/theapache64/cyclone
+  implementation("com.theapache64:cyclone:1.0.0-alpha01")
 
-    // Decompose : Decompose
-    val decomposeVersion = "0.2.5"
-    implementation("com.arkivanov.decompose:decompose-jvm:$decomposeVersion")
-    implementation("com.arkivanov.decompose:extensions-compose-jetbrains-jvm:$decomposeVersion")
+  // Decompose : Decompose
+  val decomposeVersion = "0.2.5"
+  implementation("com.arkivanov.decompose:decompose-jvm:$decomposeVersion")
+  implementation("com.arkivanov.decompose:extensions-compose-jetbrains-jvm:$decomposeVersion")
 
-    //super ellipse
-    /*api("com.github.racra:smooth-corner-rect-android-compose:v1.0.0")*/
+  //super ellipse
+  /*api("com.github.racra:smooth-corner-rect-android-compose:v1.0.0")*/
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.moshi:moshi:1.13.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+  implementation("com.squareup.moshi:moshi:1.13.0")
+  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
 
-    implementation("com.squareup.okhttp:okhttp:2.7.5")
+  implementation("com.squareup.okhttp:okhttp:2.7.5")
 
 //    //datepicker
 //    implementation ("io.github.vanpra.compose-material-dialogs:datetime:0.8.1-rc")
 
-/*    implementation ("com.squareup.okhttp3:okhttp-bom:4.10.0")
-    implementation ("com.squareup.okhttp3:okhttp")
-    implementation ("com.squareup.okhttp3:logging-interceptor")*/
+  /*    implementation ("com.squareup.okhttp3:okhttp-bom:4.10.0")
+      implementation ("com.squareup.okhttp3:okhttp")
+      implementation ("com.squareup.okhttp3:logging-interceptor")*/
 
-    /**
-     * Testing Dependencies
-     */
-    testImplementation("org.mockito:mockito-inline:4.7.0")
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+  /**
+   * Testing Dependencies
+   */
+  testImplementation("org.mockito:mockito-inline:4.7.0")
+  testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 
-    // DaggerMock
-    testImplementation("com.github.fabioCollini.daggermock:daggermock:0.8.5")
-    testImplementation("com.github.fabioCollini.daggermock:daggermock-kotlin:0.8.5")
+  // DaggerMock
+  testImplementation("com.github.fabioCollini.daggermock:daggermock:0.8.5")
+  testImplementation("com.github.fabioCollini.daggermock:daggermock-kotlin:0.8.5")
 
-    // Mockito Core : Mockito mock objects library core API and implementation
-    testImplementation("org.mockito:mockito-core:4.7.0")
+  // Mockito Core : Mockito mock objects library core API and implementation
+  testImplementation("org.mockito:mockito-core:4.7.0")
 
-    // Expekt : An assertion library for Kotlin
-    testImplementation("com.github.theapache64:expekt:1.0.0")
+  // Expekt : An assertion library for Kotlin
+  testImplementation("com.github.theapache64:expekt:1.0.0")
 
-    // JUnit
+  // JUnit
 
-    // Kotlinx Coroutines Test : Coroutines support libraries for Kotlin
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    testImplementation(compose("org.jetbrains.compose.ui:ui-test-junit4"))
+  // Kotlinx Coroutines Test : Coroutines support libraries for Kotlin
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+  testImplementation(compose("org.jetbrains.compose.ui:ui-test-junit4"))
 
-    // JUnit : JUnit is a unit testing framework for Java, created by Erich Gamma and Kent Beck.
-    testImplementation(kotlin("test-junit5"))
+  // JUnit : JUnit is a unit testing framework for Java, created by Erich Gamma and Kent Beck.
+  testImplementation(kotlin("test-junit5"))
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+  kotlinOptions.jvmTarget = "11"
 }
 
 compose.desktop {
-    application {
-        mainClass = "com.myapp.AppKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "Actify"
-            packageVersion = "1.0.0"
+  application {
+    mainClass = "com.myapp.AppKt"
+    javaHome = System.getenv("JDK_15")
+    nativeDistributions {
 
-            jvmArgs("-Dapple.awt.application.appearance=system")
+      targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+      packageName = "Actify"
+      packageVersion = "1.0.0"
+      version = "1.0.0"
+      description = "Actify cashbox application builded with JetPack Compose"
+      copyright = "© 2022 e30n3@yandex.ru Ivan Zaytsev. All rights reserved."
+      vendor = "Involta"
 
-            val iconsRoot = project.file("src/main/resources/drawables")
+      jvmArgs("-Dapple.awt.application.appearance=system")
 
-            linux {
-                iconFile.set(iconsRoot.resolve("launcher_icons/linux.png"))
-            }
+      val iconsRoot = project.file("src/main/resources/drawables")
 
-            windows {
-                iconFile.set(iconsRoot.resolve("launcher_icons/windows.ico"))
-            }
+      linux {
+        iconFile.set(iconsRoot.resolve("launcher_icons/linux.png"))
+      }
 
-            macOS {
-                iconFile.set(iconsRoot.resolve("launcher_icons/macos.icns"))
-            }
+      windows {
+        iconFile.set(iconsRoot.resolve("launcher_icons/windows.ico"))
+        dirChooser = true
+      }
 
-        }
+      macOS {
+        iconFile.set(iconsRoot.resolve("launcher_icons/macos.icns"))
+      }
+
     }
+  }
 }
