c097ef8f7e18f1f026dbabd550a8bcf39ac1ce47
Ivan Zaytsev <i.zajcev@involta.ru>
2023-05-01 16:18:23 +0300

    feat: transaction name from api

--- src/main/kotlin/com/myapp/actify/domain/entity/api/response/ReportResponse.kt
+++ src/main/kotlin/com/myapp/actify/domain/entity/api/response/ReportResponse.kt
@@ -6,79 +6,81 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ReportResponse(
-  val isSuccess: Boolean,
-  val data: List<ReportBody> = emptyList(),
-  val message: String = "Проверьте корректность введенных даных"
+    val isSuccess: Boolean,
+    val data: List<ReportBody> = emptyList(),
+    val message: String = "Проверьте корректность введенных даных"
 )
 
 @JsonClass(generateAdapter = true)
 data class ReportBody(
-  @Json(name = "id")
-  val id: Long,
-  @Json(name = "guid")
-  val guid: String,
-  @Json(name = "type_id")
-  val typeId: Long,
-  @Json(name = "date")
-  val date: String,
-  @Json(name = "card")
-  val card: String,
-  @Json(name = "is_cancelled")
-  val iCancelled: Boolean,
-  @Json(name = "is_active")
-  val isActive: Boolean,
-  @Json(name = "totalBonus")
-  val totalBonus: TotalBonusBody,
-  @Json(name = "totalAmount")
-  val totalAmount: TotalAmountBody,
-  @Json(name = "operations")
-  val operations: List<OperationBody>,
+    @Json(name = "id")
+    val id: Long,
+    @Json(name = "guid")
+    val guid: String,
+    @Json(name = "type_id")
+    val typeId: Long,
+    @Json(name = "date")
+    val date: String,
+    @Json(name = "card")
+    val card: String,
+    @Json(name = "is_cancelled")
+    val iCancelled: Boolean,
+    @Json(name = "is_active")
+    val isActive: Boolean,
+    @Json(name = "totalBonus")
+    val totalBonus: TotalBonusBody,
+    @Json(name = "totalAmount")
+    val totalAmount: TotalAmountBody,
+    @Json(name = "operations")
+    val operations: List<OperationBody>,
+    @Json(name = "transaction_name")
+    val transactionName: String,
 )
 
 @JsonClass(generateAdapter = true)
 data class TotalBonusBody(
-  @Json(name = "1")
-  val value: Double,
+    @Json(name = "1")
+    val value: Double,
 )
 
 @JsonClass(generateAdapter = true)
 data class TotalAmountBody(
-  @Json(name = "1")
-  val value: Double,
+    @Json(name = "1")
+    val value: Double,
 )
 
 @JsonClass(generateAdapter = true)
 data class OperationBody(
-  @Json(name = "bonus_type")
-  val bonusType: String,
-  @Json(name = "category_id")
-  val categoryId: Long,
-  @Json(name = "bonus")
-  val bonus: Double,
-  @Json(name = "is_active")
-  val isActive: Boolean,
-  @Json(name = "action_id")
-  val actionId: Long?,
-  @Json(name = "action")
-  val action: String?,
+    @Json(name = "bonus_type")
+    val bonusType: String,
+    @Json(name = "category_id")
+    val categoryId: Long,
+    @Json(name = "bonus")
+    val bonus: Double,
+    @Json(name = "is_active")
+    val isActive: Boolean,
+    @Json(name = "action_id")
+    val actionId: Long?,
+    @Json(name = "action")
+    val action: String?,
 ) {
-  val bonusTypeToString: String
-    get() =
-      when (bonusType) {
-        "BASIC" -> "Универсальные"
-        "COMPANY" -> "Фирменные"
-        else -> bonusType
-      }
+    val bonusTypeToString: String
+        get() =
+            when (bonusType) {
+                "BASIC" -> "Универсальные"
+                "COMPANY" -> "Фирменные"
+                else -> bonusType
+            }
 
 }
 
 
 @JsonClass(generateAdapter = true)
 data class ReportElement(
-  @Json(name = "prize")
-  val prize: String,
-  @Json(name = "for")
-  val description: String,
-  @Json(name = "prizeId")
-  val prizeId: Long
+    @Json(name = "prize")
+    val prize: String,
+    @Json(name = "for")
+    val description: String,
+    @Json(name = "prizeId")
+    val prizeId: Long
 )
--- src/main/kotlin/com/myapp/ui/feature/drawer/report/ReportScreen.kt
+++ src/main/kotlin/com/myapp/ui/feature/drawer/report/ReportScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.myapp.ui.element.DatePicker
-import com.myapp.util.ShimmerBrush
 import com.myapp.util.extention.getCurrentDate
 import com.myapp.util.extention.prettyString
 import ru.involta.actify.domain.Result
@@ -30,208 +29,203 @@ import ru.involta.actify.util.extention.shimmer
 
 @Composable
 fun ReportScreen(viewModel: ReportViewModel, modifier: Modifier = Modifier) {
-  val def = remember { 16.dp }
-  val reportState = viewModel.stateReport.collectAsState()
+    val def = remember { 16.dp }
+    val reportState = viewModel.stateReport.collectAsState()
 
-  LaunchedEffect(reportState.value.status) {
-    if (reportState.value.status == Result.Status.EMPTY) viewModel.report()
-  }
-
-  Column(Modifier.fillMaxSize()) {
-    Surface(color = MaterialTheme.colors.secondary) {
-      Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-          .padding(horizontal = def, vertical = def / 4)
-          .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-      ) {
-        Text(text = "Отчёт", style = MaterialTheme.typography.h6)
-        Row() {
-          IconButton(onClick = { viewModel.report() }) {
-            Icon(imageVector = Icons.Filled.Refresh, contentDescription = "")
-          }
-          val selectedDate = rememberSaveable { mutableStateOf(getCurrentDate()) }
-          val isOpen = rememberSaveable { mutableStateOf(false) }
-          DatePicker(isOpen.value, { isOpen.value = false }, selectedDate.value) {
-            selectedDate.value = it
-            viewModel.report(selectedDate.value)
-          }
-          IconButton(onClick = { isOpen.value = true }) {
-            Icon(imageVector = Icons.Filled.EditCalendar, contentDescription = "")
-          }
-        }
-      }
+    LaunchedEffect(reportState.value.status) {
+        if (reportState.value.status == Result.Status.EMPTY) viewModel.report()
     }
 
-    Crossfade(targetState = reportState.value.status, modifier = Modifier.fillMaxSize()) {
-      when (it) {
-        Result.Status.SUCCESS -> reportState.value.data?.let { data ->
-          if (data.isEmpty())
-            Text(text = "В этот день не было транзакций", Modifier.padding(def))
-          else
-            LazyColumn(modifier) {
-              items(data.reversed()) { element ->
-                ReportItem(r = element)
-              }
+    Column(Modifier.fillMaxSize()) {
+        Surface(color = MaterialTheme.colors.secondary) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(horizontal = def, vertical = def / 4)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Отчёт", style = MaterialTheme.typography.h6)
+                Row() {
+                    IconButton(onClick = { viewModel.report() }) {
+                        Icon(imageVector = Icons.Filled.Refresh, contentDescription = "")
+                    }
+                    val selectedDate = rememberSaveable { mutableStateOf(getCurrentDate()) }
+                    val isOpen = rememberSaveable { mutableStateOf(false) }
+                    DatePicker(isOpen.value, { isOpen.value = false }, selectedDate.value) {
+                        selectedDate.value = it
+                        viewModel.report(selectedDate.value)
+                    }
+                    IconButton(onClick = { isOpen.value = true }) {
+                        Icon(imageVector = Icons.Filled.EditCalendar, contentDescription = "")
+                    }
+                }
             }
         }
 
-        Result.Status.ERROR -> {
-          Text(text = reportState.value.exception?.message ?: "", Modifier.padding(def))
-        }
+        Crossfade(targetState = reportState.value.status, modifier = Modifier.fillMaxSize()) {
+            when (it) {
+                Result.Status.SUCCESS -> reportState.value.data?.let { data ->
+                    if (data.isEmpty())
+                        Text(text = "В этот день не было транзакций", Modifier.padding(def))
+                    else
+                        LazyColumn(modifier) {
+                            items(data.reversed()) { element ->
+                                ReportItem(r = element)
+                            }
+                        }
+                }
 
-        Result.Status.LOADING -> {
+                Result.Status.ERROR -> {
+                    Text(text = reportState.value.exception?.message ?: "", Modifier.padding(def))
+                }
 
-          LazyColumn(modifier) {
-            items(100) {
-              ReportItemShimmer()
+                Result.Status.LOADING -> {
+
+                    LazyColumn(modifier) {
+                        items(100) {
+                            ReportItemShimmer()
+                        }
+                    }
+
+                    /* Text(
+                       "4234",
+                       modifier = Modifier
+                         .shimmer()
+                         .fillMaxWidth()
+                         .height(500.dp)
+                         .shimmer(),
+                       textAlign = TextAlign.Center
+                     )*/
+                }
+
+                Result.Status.EMPTY -> {
+
+                }
             }
-          }
-
-          /* Text(
-             "4234",
-             modifier = Modifier
-               .shimmer()
-               .fillMaxWidth()
-               .height(500.dp)
-               .shimmer(),
-             textAlign = TextAlign.Center
-           )*/
         }
-
-        Result.Status.EMPTY -> {
-
-        }
-      }
     }
-  }
 }
 
 @Composable
 fun ReportItemShimmer() {
-  val r = ReportBody(
-    0,
-    "",
-    0,
-    "",
-    "",
-    false,
-    false,
-    TotalBonusBody(0.0),
-    TotalAmountBody(0.0),
-    listOf(OperationBody("", 0, 0.0, false, null, null), OperationBody("", 0, 0.0, false, null, null))
-  )
-  Column {
-    Column(
-      Modifier
-        .fillMaxWidth()
-        .padding(16.dp)
-        .shimmer()
-    ) {
-      Column() {
-        Text(
-          text = "",
-          style = MaterialTheme.typography.h5,
-          modifier = Modifier.fillMaxWidth(),
-          textAlign = TextAlign.Center
-        )
-        Text(
-          text = "",
-          modifier = Modifier.fillMaxWidth(),
-          textAlign = TextAlign.Center,
-          fontSize = 14.sp,
-          fontWeight = FontWeight.Light
-        )
-      }
-      Spacer(modifier = Modifier.height(8.dp))
-      Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-        Text(text = "")
-        Text(text = "")
-      }
-      Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-        Text(text = "")
-        Text(text = "")
-      }
-      Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-        Text(text = "")
-        Text(text = "")
-      }
-      Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-        Text(text = "")
-        Text(text = "")
-      }
-      Spacer(modifier = Modifier.height(8.dp))
-      Text(text = "", style = MaterialTheme.typography.h5)
-      r.operations.forEach {
-        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-          Text(text = "")
-          Text(text = "")
+    val r = ReportBody(
+        0,
+        "",
+        0,
+        "",
+        "",
+        false,
+        false,
+        TotalBonusBody(0.0),
+        TotalAmountBody(0.0),
+        listOf(OperationBody("", 0, 0.0, false, null, null), OperationBody("", 0, 0.0, false, null, null)),
+        ""
+    )
+    Column {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .shimmer()
+        ) {
+            Column() {
+                Text(
+                    text = "",
+                    style = MaterialTheme.typography.h5,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Light
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "")
+                Text(text = "")
+            }
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "")
+                Text(text = "")
+            }
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "")
+                Text(text = "")
+            }
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "")
+                Text(text = "")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "", style = MaterialTheme.typography.h5)
+            r.operations.forEach {
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "")
+                    Text(text = "")
+                }
+            }
         }
-      }
+        Divider()
     }
-    Divider()
-  }
 
 }
 
 @Composable
 fun ReportItem(r: ReportBody) {
-  SelectionContainer {
-    Column {
-      Column(
-        Modifier
-          .fillMaxWidth()
-          .padding(16.dp)
-      ) {
-        Column() {
-          Text(
-            text = "${
-              when (r.typeId) {
-                1L -> "Списание"
-                2L -> "Начисление"
-                else -> "Выдача приза"
-              }
-            } № ${r.id}",
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center
-          )
-          Text(
-            text = r.guid,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Light
-          )
+    SelectionContainer {
+        Column {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Column() {
+                    Text(
+                        text = "${r.transactionName} № ${r.id}",
+                        style = MaterialTheme.typography.h5,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = r.guid,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Light
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Дата")
+                    Text(text = r.date)
+                }
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Номер карты")
+                    Text(text = r.card)
+                }
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Сумма")
+                    Text(text = r.totalAmount.value.prettyString)
+                }
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Бонусы")
+                    Text(text = r.totalBonus.value.prettyString)
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Детали бонусов", style = MaterialTheme.typography.h5)
+                r.operations.forEach {
+                    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                        Text(text = it.bonusTypeToString)
+                        Text(text = it.bonus.prettyString)
+                    }
+                }
+            }
+            Divider()
         }
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-          Text(text = "Дата")
-          Text(text = r.date)
-        }
-        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-          Text(text = "Номер карты")
-          Text(text = r.card)
-        }
-        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-          Text(text = "Сумма")
-          Text(text = r.totalAmount.value.prettyString)
-        }
-        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-          Text(text = "Бонусы")
-          Text(text = r.totalBonus.value.prettyString)
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Детали бонусов", style = MaterialTheme.typography.h5)
-        r.operations.forEach {
-          Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-            Text(text = it.bonusTypeToString)
-            Text(text = it.bonus.prettyString)
-          }
-        }
-      }
-      Divider()
     }
-  }
 }
